7e6109ad39d3d7f35eed14e82ca566f5af339b43
garamieeeeee <bkb120996@gmail.com>
2025-02-11 22:54:39 +0900

    Create main.c

---
+++ 구현 코드/우선순위 큐/힙/최소힙/main.c
@@ -0,0 +1,15 @@
+#include "minHeap.h"
+
+void test() {
+	ElementType list[8] = { 23, 56, 11, 9, 56, 99, 27, 34 };
+	HeapSort(list, 8);
+	for (int i = 0; i < 8; i++) {
+		printf("%d ", list[i].key);
+	}
+	printf("\n");
+}
+
+int main() {
+	test();
+	return 0;
+}
