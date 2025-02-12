16dbb6136093947e62ab0ba6a91cf0a9cf426dd8
garamieeeeee <bkb120996@gmail.com>
2025-02-12 19:11:20 +0900

    Update ArrayCircularDeque.h

--- 구현 코드/덱/1차원 배열로 구현한 덱/원형 덱/ArrayCircularDeque.h
+++ 구현 코드/덱/1차원 배열로 구현한 덱/원형 덱/ArrayCircularDeque.h
@@ -1,29 +1,29 @@
-#ifndef DEQUE_H
-#define DEQUE_H
+#ifndef ARRAYCIRCULARDEQUE_H
+#define ARRAYCIRCULARDEQUE_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX_QUEUE_SIZE 5
 
-typedef int element;
+typedef int ElementType;
 
 typedef struct {
-	int front;
-	int rear;
-	element data[MAX_QUEUE_SIZE];
+	int front, rear;
+	ElementType deque[MAX_QUEUE_SIZE];
 }DequeType;
 
 void Error(char* message);
-void InitDeque(DequeType* dq);
+DequeType* CreateDeque();
 int IsEmpty(DequeType* dq);
 int IsFull(DequeType* dq);
 void PrintDequeue(DequeType* dq);
-void AddRear(DequeType* dq, element data);
-element DeleteFront(DequeType* dq);
-element GetFront(DequeType* dq);
-void AddFront(DequeType* dq, element data);
-element DeleteRear(DequeType* dq);
-element GetRear(DequeType* dq);
+void AddRear(DequeType* dq, ElementType data);
+ElementType DeleteFront(DequeType* dq);
+ElementType GetFront(DequeType* dq);
+void AddFront(DequeType* dq, ElementType data);
+ElementType DeleteRear(DequeType* dq);
+ElementType GetRear(DequeType* dq);
+void DestroyDeque(DequeType** dq);
 
 #endif
