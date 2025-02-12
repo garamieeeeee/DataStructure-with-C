7806757e0bbb11a700333dd46f556dff286b5b63
garamieeeeee <bkb120996@gmail.com>
2025-02-12 16:19:38 +0900

    Create main.c

---
+++ 구현 코드/큐/동적할당 배열을사용한 큐/선형큐/main.c
@@ -0,0 +1,70 @@
+#include "DynamicArrayLinearQueue.h"
+
+void test() {
+	//초기 용량이 5인 큐 생성
+	QueueType* q = CreateQueue(5);
+	ElementType data;
+
+	Enqueue(q, 10);
+	printf("<< Enqueue 10 >>\n");
+	PrintQueue(q);
+	printf("\n");
+
+	Enqueue(q, 20);
+	printf("<< Enqueue 20 >>\n");
+	PrintQueue(q);
+	printf("\n");
+
+	Enqueue(q, 30);
+	printf("<< Enqueue 30 >>\n");
+	PrintQueue(q);
+	printf("\n");
+
+	Enqueue(q, 40);
+	printf("<< Enqueue 40 >>\n");
+	PrintQueue(q);
+	printf("\n");
+
+	Enqueue(q, 50);
+	printf("<< Enqueue 50 >>\n");
+	PrintQueue(q);
+	printf("\n");
+
+	printf("queue capacity: %d\n", q->capacity); printf("\n");
+
+	Enqueue(q, 60);
+	printf("<< Enqueue 60 >>\n");
+	PrintQueue(q);
+	printf("\n");
+
+	Enqueue(q, 70);
+	printf("<< Enqueue 70 >>\n");
+	PrintQueue(q);
+	printf("\n");
+	printf("queue capacity: %d\n", q->capacity); printf("\n");
+	//큐의 용량이 2배로 확장됨
+
+	data = Dequeue(q);
+	printf("<< Dequeue >>\n");
+	PrintQueue(q);
+	printf("\n");
+
+	data = Dequeue(q);
+	printf("<< Dequeue >>\n");
+	PrintQueue(q);
+	printf("\n");
+
+	data = Dequeue(q);
+	printf("<< Dequeue >>\n");
+	PrintQueue(q);
+	printf("\n");
+
+
+	printf("큐 삭제\n"); DestroyQueue(&q);
+	if (q) printf("큐가 삭제되지 않았습니다.\n");
+}
+
+int main() {
+	test();
+	return 0;
+}
