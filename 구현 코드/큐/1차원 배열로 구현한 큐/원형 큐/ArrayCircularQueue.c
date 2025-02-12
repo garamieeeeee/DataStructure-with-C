506362242bb2fd73106ee4e89363b404d4822028
garamieeeeee <bkb120996@gmail.com>
2025-02-12 16:08:40 +0900

    Update ArrayCircularQueue.c

--- 구현 코드/큐/1차원 배열로 구현한 큐/원형 큐/ArrayCircularQueue.c
+++ 구현 코드/큐/1차원 배열로 구현한 큐/원형 큐/ArrayCircularQueue.c
@@ -1,23 +1,46 @@
-#include "CircularQueue.h"
+#include "ArrayCircularQueue.h"
 
 void Error(char* message) {
 	fprintf(stderr, "%s\n", message);
 	exit(EXIT_FAILURE);
 }
 
-void InitQueue(QueueType* q) {
-	q->front = 0;
-	q->rear = 0;
+QueueType* CreateQueue() { //큐 구조체를 생성하고 초기화하여 반환
+	QueueType* q = (QueueType*)malloc(sizeof(QueueType));
+	if (!q) Error("memory allocation failed.");
+	q->front = q->rear = 0;
+	return q;
+}
 
-	return;
+int IsEmpty(QueueType* q) {
+	return (q->front == q->rear);
 }
 
 int IsFull(QueueType* q) {
 	return ((q->rear + 1) % MAX_QUEUE_SIZE == q->front);
 }
 
-int IsEmpty(QueueType* q) {
-	return (q->front == q->rear);
+void Enqueue(QueueType* q, ElementType data) {
+	if (IsFull(q)) Error("Queue is full.");
+	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+	q->queue[q->rear] = data;
+}
+
+ElementType Dequeue(QueueType* q) {
+	if (IsEmpty(q)) Error("Queue is empty.");
+	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+	return q->queue[q->front];
+}
+
+ElementType Peek(QueueType* q) {
+	if (IsEmpty(q)) Error("Queue is empty.");
+	return q->queue[(q->front + 1) % MAX_QUEUE_SIZE];
+}
+
+void DestroyQueue(QueueType** q) {
+	if (*q == NULL) return;
+	free(*q);
+	*q = NULL;
 }
 
 void PrintQueue(QueueType* q) {
@@ -26,42 +49,11 @@ void PrintQueue(QueueType* q) {
 		int i = q->front;
 		do {
 			i = (i + 1) % MAX_QUEUE_SIZE;
-			printf("[%d] %d | ", i, q->data[i]);
+			printf("[%d] %d | ", i, q->queue[i]);
 			if (i == q->rear) {
 				break;
 			}
 		} while (i != q->front);
 	}
 	printf("\n");
-}
-
-void Enqueue(QueueType* q, element data) {
-	if (IsFull(q)) {
-		Error("Queue is Full\n");
-		return;
-	}
-
-	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
-	q->data[q->rear] = data;
-
-	return;
-}
-
-element Dequeue(QueueType* q) {
-	if (IsEmpty(q)) {
-		Error("Queue is Empty\n");
-		return -1;
-	}
-
-	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
-	return q->data[q->front];
-}
-
-element Peek(QueueType* q) {
-	if (IsEmpty(q)) {
-		Error("Queue is Empty\n");
-		return -1;
-	}
-
-	return q->data[(q->front + 1) % MAX_QUEUE_SIZE];
 }
