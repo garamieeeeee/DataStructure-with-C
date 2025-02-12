6851681d7d36d888447f732f5aa4c01dfb58172e
garamieeeeee <bkb120996@gmail.com>
2025-02-12 16:34:42 +0900

    Create DynamicArrayCircularQueue.h

---
+++ 구현 코드/큐/동적할당 배열을사용한 큐/원형큐/DynamicArrayCircularQueue.h
@@ -0,0 +1,26 @@
+#ifndef DYNAMICARRAYCIRCULARQUEUE_H
+#define DYNAMICARRAYCIRCULARQUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+
+typedef struct {
+	ElementType* queue;
+	int front, rear;
+	int capacity;
+}QueueType;
+
+void Error(char* message);
+QueueType* CreateQueue(int capacity);
+int IsEmpty(QueueType* q);
+int IsFull(QueueType* q);
+void resizeQueue(QueueType* q);
+void Enqueue(QueueType* q, ElementType data);
+ElementType Dequeue(QueueType* q);
+ElementType Peek(QueueType* q);
+void DestroyQueue(QueueType** q);
+void PrintQueue(QueueType* q);
+
+#endif
